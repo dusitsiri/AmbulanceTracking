995acee1f9db2c69fbc111a7fde2bdbef2ec3733
dusitsiri <dusit.si@ku.th>
2019-01-22 00:06:51 +0700

    Monitoring LoRa signal on an ambulance with Mobile Application

---
+++ app/src/main/java/th/ac/ku/ambulancetracking/RegisterAppActivity.java
@@ -0,0 +1,13 @@
+package th.ac.ku.ambulancetracking;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class RegisterAppActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registers_app_activity);
+    }
+}
